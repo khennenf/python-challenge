a0a3215f678425ff2c21820746a4250475a333ff
Keri <keri@rogue.internal>
2021-01-09 15:34:47 -0500

    changes

--- PyPoll/practice files/FileBreakdown.py
+++ PyPoll/practice files/FileBreakdown.py
@@ -6,9 +6,11 @@
 # print(hot_days)
 
 import os
+from collections import OrderedDict
+import math
 import csv
 ##### Counts the rows
-path = os.path.join("..", "Resources", "election_data_short.csv")
+path = os.path.join("..", "Resources", "election_data.csv")
 
 c1 = []
 c2 = []
@@ -21,8 +23,7 @@ with open(path) as electiondatafile:
         c1.append(row[0])
         c2.append(row[1])
         c3.append(row[2])
-        print (f"printingValues----------------------------------")
-#   
+        #   
 #   for county in c2:
 #         print(county)
 #     res = {} 
@@ -40,18 +41,52 @@ with open(path) as electiondatafile:
 #     print(v)
 # for county in c2:
 #     print(county)
-
-#     # not_marsh_votes = []
-#     # for county in c2:
-#     #     if county != "Marsh":
-#     #         not_marsh_votes.append(county)
-#     # print(not_marsh_votes)
-#     # candidatevotes = []
-#     # for candidate in c3:
-#     #     if candidate != "Khan":
-#     #         candidatevotes.append(candidate)
-#     # print (f"printinglast----------------------------------")            
-#     # print(f"{candidatevotes} {not_marsh_votes}")
+    # not_marsh_votes = []
+    # for county in c2:
+    #     if county != "Marsh":
+    #         not_marsh_votes.append(county)
+    # print(not_marsh_votes)
+    # unique = OrderedDict((x,1) for x in c3).keys()
+    # for candidatename in unique:
+    #         print(candidatename)
+    # print(unique)
+    print(f"```")
+    print(f"Election Results")
+    print(f"-------------------------")
+    totalvotes = len(c1)
+    print(f"Total Votes: {totalvotes}")
+    print(f"-------------------------")
+    uniquenames = set(c3)
+    for item in uniquenames:
+        print(f"{item}: {'%.4f'%(float((c3.count(item))/(totalvotes))*100)}% ({(c3.count(item))}) ")
+    def most_frequent(c3): 
+        return max(set(c3), key = c3.count) 
+    print(f"-------------------------")
+    print(f"Winner: {(most_frequent(c3))}") 
+    print(f"-------------------------")
+    print(f"```")
+    # candidatevotes = []
+    # for candidate in c3:
+    #     if candidate == "Khan":
+    #         candidatevotes.append(candidate)
+    #     # print(candidatevotes)
+    # print (f"printinglast----------------------------------")    
+    # # a = unique
+    # # b = c3
+    # # for x, y in [(x,y) for x in b for x in a]:
+    # #     print(f"printing_____________ {x}")        
+    # # # print(f"{candidatevotes} {not_marsh_votes}")
+    # khanvotes = len(candidatevotes)
+    # print(f"Votes for Khan: {khanvotes}")
+    # totalvotes = len(c1)
+    # print (f"Total votes are: {totalvotes}")
+    # khanpercent = khanvotes/totalvotes
+    # print(khanpercent)
+    # khannumpercent = ((khanpercent)*100)
+    # print(round(khannumpercent, 4))
+    # khannumpercentrnd = round(khannumpercent, 4)
+    # print(f"Percentage for Khan is {khannumpercentrnd}")
+    # print ("{0:.4f}". format(khannumpercent)) #will add 0 after decimal point for int values.
 
 #     # # initializing lists 
 # # test_keys = ["Rash", "Kil", "Varsha"] 
--- PyPoll/practice files/winner.py
+++ PyPoll/practice files/winner.py
@@ -1,6 +1,7 @@
 #* The winner of the election based on popular vote.
 #Winner: Khan
 import os
+from collections import OrderedDict
 import csv
 path = os.path.join("..", "Resources", "election_data_short.csv")
 
@@ -22,17 +23,47 @@ with open(path) as electiondatafile:
         c2.append(row[1])
         c3.append(row[2])
     print (f"printingValues----------------------------------")
-    for county in c2:
-        print(county)
-    for i in range(len(c2)):
-        print(c2[i])
-    # mylistint = list(map(int, c2))
-    # for x in range(mylistint):
-    #     print(f"This is a {x}")
+    # for county in c2:
+    #     print(county)
+    # print(c1)
+    # print(c2)
+    # print(c3)
+c1int = list(map(int, c1)) ###Changes list to int
+a = [c1int] 
+b = [c2]  
+c = [c3]  
 
-#     # for row in csvreader:
-#     #     print(row)
-# numbers = [ "k", "k", "k", 22, 90, 12, 2]
+result = zip(a, b, c)
+print(result)
+
+new_dict_3 = {result}
+print(new_dict_3)
+# for a in result:
+#     print(f"This is {a} ; This is {b}")
+
+# new_dict = dict(zip(c1int, c2))
+# print(new_dict)
+
+# new_dict_2 = dict(zip(new_dict, c3))
+# print(new_dict_2)
+
+# for i in new_dict_2:
+#     print(i, j)
+
+# new_dict = {}
+# i:[j, k] for i, j, k in zip(a, b, c)}
+
+# valuesdict = {} 
+# for month in c1: 
+#     for values in diff_list: 
+#         valuesdict[month] = values 
+#         diff_list.remove(values) 
+
+
+
+    # for row in csvreader:
+    #     print(row)
+# numbers = [ c1]
 # def get_unique_numbers(numbers):
 #     list_of_unique_numbers = []
 #     unique_numbers = set(numbers)
@@ -53,3 +84,9 @@ with open(path) as electiondatafile:
 #     print(f"{name}")
 # for x in numlist:
 #     print(f"This is a {x}")
+
+# votesdict = {}
+# for votes in c1: 
+#     for values in diff_list: 
+#         valuesdict[month] = values 
+#         diff_list.remove(values) 
