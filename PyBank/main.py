0646ca3cd56cdf532b3357bcb7737085c12f7040
Keri <keri@rogue.internal>
2021-01-12 13:29:14 -0500

    added correct version of sum min max

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,17 @@
 import os
 import csv
 import sys
+#File Path
 path = os.path.join("..", "Resources", "budget_data.csv")
+#Declare columns to lists
 c1 = []
 c2 = []
+#Open File
 with open(path) as budgetfile:
     csvreader = csv.reader(budgetfile, delimiter=",")
     csv_header = next(csvreader) 
     for row in csvreader:
+        #(add rows to columns)
         c1.append(row[0])
         c2.append(row[1])
 c2int = list(map(int, c2)) ###Changes list to int
@@ -15,11 +19,14 @@ total_months = len(c1)
 total_amount = sum(c2int)
 months_average = total_months - 1
 diff_list = []
+#Get difference between months
 for i in range(1,len(c2int)):
     x = c2int[i] - c2int[i-1]
     diff_list.append(x)
 sum_month_change = sum(diff_list)
+#delete first row
 c1.pop(0)
+#create dict of items
 valuesdict = {} 
 for month in c1: 
     for values in diff_list: 
@@ -30,47 +37,28 @@ maxvalue = (max(valuesdict.items(), key=lambda x: x[1]))
 mmax, vmax  = maxvalue
 mmin, vmin = minvalue
 average_change = sum_month_change/months_average
-average_change_decimal = round(average_change, 2)
+average_change_decimal = round(average_change, 2) 
 print(f"```text")
 print(f"Financial Analysis")
 print(f"----------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total$: ${total_amount}")
+print(f"Total: ${total_amount}")
 print(f"Average Change: ${average_change_decimal}")
 print(f"Greatest Increase in Profits: {mmax} (${vmax})")
 print(f"Greatest Decrease in Profits: {mmin} (${vmin})")
 print(f"```")
-
-sys.stdout = open('output3.txt', 'wt') 
-print(f"```text")
-print(f"Financial Analysis")
-print(f"----------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total$: ${total_amount}")
-print(f"Average Change: ${average_change_decimal}")
-print(f"Greatest Increase in Profits: {mmax} (${vmax})")
-print(f"Greatest Decrease in Profits: {mmin} (${vmin})")
-print(f"```")
-
 
 sys.stdout = open('/Users/keri.ROGUE/Desktop/python-challenge/PyBank/analysis/output.txt', 'wt') 
 print(f"```text")
 print(f"Financial Analysis")
 print(f"----------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total$: ${total_amount}")
+print(f"Total: ${total_amount}")
 print(f"Average Change: ${average_change_decimal}")
 print(f"Greatest Increase in Profits: {mmax} (${vmax})")
 print(f"Greatest Decrease in Profits: {mmin} (${vmin})")
 print(f"```")
 
-# with open("~keri.ROGUE/Desktop/python-challenge/PyBank/analysis/output5.txt", 'w') as txt:
-#     txt.writelines('Hello!')
-
-# c://users/keri.ROGUE/Desktop/python-challenge/PyBank/analysis
-
-# c://users/keri.ROGUE/Desktop/python-challenge/PyBank/analysis
-# output5.txt", 'w') 
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,21 +2,40 @@ import os
 from collections import OrderedDict
 import math
 import csv
-##### Counts the rows
+import sys
+##### File path
 path = os.path.join("..", "Resources", "election_data.csv")
-
+#Declare columns to lists
 c1 = []
 c2 = []
 c3 = []
 with open(path) as electiondatafile:
     csvreader = csv.reader(electiondatafile, delimiter=",")
     csv_header = next(csvreader) 
-    # print(csvreader)
+    #Add rows to columns
     for row in csvreader:
         c1.append(row[0])
         c2.append(row[1])
         c3.append(row[2])
 
+    print(f"```")
+    print(f"Election Results")
+    print(f"-------------------------")
+    totalvotes = len(c1)
+    print(f"Total Votes: {totalvotes}")
+    print(f"-------------------------")
+    uniquenames = set(c3)
+    for item in uniquenames:
+        print(f"{item}: {'%.4f'%(float((c3.count(item))/(totalvotes))*100)}% ({(c3.count(item))}) ")
+        #Printing {Item}: {Average, incluces 4 decimal points} {total count}
+    def most_frequent(c3): 
+        return max(set(c3), key = c3.count) 
+    print(f"-------------------------")
+    print(f"Winner: {(most_frequent(c3))}") 
+    print(f"-------------------------")
+    print(f"```")
+
+    sys.stdout = open('/Users/keri.ROGUE/Desktop/python-challenge/PyPoll/analysis/output.txt', 'wt') 
     print(f"```")
     print(f"Election Results")
     print(f"-------------------------")
